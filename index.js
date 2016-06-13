5637429a59c2128f9061ad624f070eac6c9fd24d
Zebulon McCorkle <zebmccorkle@gmail.com>
2016-06-13 11:03:19 -0400

    Add jsdelivr-rum to help jsDelivr

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+// jsdelivr-rum
+(function(a,b,c,d,e){function f(){var a=b.createElement("script");a.async=!0;
+a.src="//radar.cedexis.com/1/11475/radar.js";b.body.appendChild(a)}/\bMSIE 6/i
+.test(a.navigator.userAgent)||(a[c]?a[c](e,f,!1):a[d]&&a[d]("on"+e,f))})
+(window,document,"addEventListener","attachEvent","load");
+
 /*
 Copyright (c) 2016 Zebulon McCorkle
 
